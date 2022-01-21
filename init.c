1e62149b55f04ce3e27e66a4c66d8c027cfceeaa
Olexa Bilaniuk <obilaniu@gmail.com>
2022-01-21 12:05:41 -0500

    SGEMM v4: Unroll 16x and tune heavily.

--- init.c
+++ init.c
@@ -19,17 +19,17 @@
 
 
 /* Data Structure Declaration */
-typedef struct nabla_cpuid_generic_t       nabla_cpuid_t;
-typedef struct nabla_cpuid_generic_t       nabla_cpuid_generic_t;
-typedef struct nabla_cpuid_0000000b_t      nabla_cpuid_0000000b_t;
-typedef struct nabla_cpuid_80000001_amd_t  nabla_cpuid_80000001_amd_t;
-typedef struct nabla_cpuid_80000008_amd_t  nabla_cpuid_80000008_amd_t;
-typedef struct nabla_cpuid_8000001d_amd_t  nabla_cpuid_8000001d_amd_t;
-typedef struct nabla_cpuid_8000001e_amd_t  nabla_cpuid_8000001e_amd_t;
-struct nabla_cpuid_generic_t{
+typedef struct nabla_x86_cpuid_generic_t       nabla_x86_cpuid_t;
+typedef struct nabla_x86_cpuid_generic_t       nabla_x86_cpuid_generic_t;
+typedef struct nabla_x86_cpuid_0000000b_t      nabla_x86_cpuid_0000000b_t;
+typedef struct nabla_x86_cpuid_80000001_amd_t  nabla_x86_cpuid_80000001_amd_t;
+typedef struct nabla_x86_cpuid_80000008_amd_t  nabla_x86_cpuid_80000008_amd_t;
+typedef struct nabla_x86_cpuid_8000001d_amd_t  nabla_x86_cpuid_8000001d_amd_t;
+typedef struct nabla_x86_cpuid_8000001e_amd_t  nabla_x86_cpuid_8000001e_amd_t;
+struct nabla_x86_cpuid_generic_t{
     uint32_t eax, ebx, ecx, edx;
 };
-struct nabla_cpuid_0000000b_t{
+struct nabla_x86_cpuid_0000000b_t{
     /* EAX */
     uint32_t next_level_shift  :  5;
     uint32_t _pad0             : 27;
@@ -43,7 +43,7 @@ struct nabla_cpuid_0000000b_t{
     /* EDX */
     uint32_t ext_apic_id;
 };
-struct nabla_cpuid_80000001_amd_t{
+struct nabla_x86_cpuid_80000001_amd_t{
     /* EAX */
     uint32_t stepping          :  4;
     uint32_t base_model        :  4;
@@ -122,7 +122,7 @@ struct nabla_cpuid_80000001_amd_t{
     uint32_t threednowext      :  1; /* 3DNow! Extensions */
     uint32_t threednow         :  1; /* 3DNow! */
 };
-struct nabla_cpuid_80000008_amd_t{
+struct nabla_x86_cpuid_80000008_amd_t{
     /* EAX */
     uint32_t phys_addr_size         :  8;
     uint32_t lin_addr_size          :  8;
@@ -167,7 +167,7 @@ struct nabla_cpuid_80000008_amd_t{
     uint32_t rdpru_max              :  8;/* Maximum input in ECX to RDPRU instruction. */
     uint32_t _pad12                 :  8;
 };
-struct nabla_cpuid_8000001d_amd_t{
+struct nabla_x86_cpuid_8000001d_amd_t{
     /* EAX */
     uint32_t type        :  5;
     uint32_t level       :  3;
@@ -187,7 +187,7 @@ struct nabla_cpuid_8000001d_amd_t{
     uint32_t inclusive   :  1;
     uint32_t _pad2       : 30;
 };
-struct nabla_cpuid_8000001e_amd_t{
+struct nabla_x86_cpuid_8000001e_amd_t{
     /* EAX */
     uint32_t ext_apic_id;
     /* EBX */
@@ -207,18 +207,18 @@ struct nabla_cpuid_8000001e_amd_t{
 /* Static Asserts */
 _Static_assert(sizeof(pthread_mutex_t) <= CL_SIZE,
                "pthread mutex larger than a cacheline!");
-_Static_assert(sizeof(nabla_cpuid_generic_t) == 16,
-               "struct nabla_cpuid_generic_t must be exactly 16 bytes!");
-_Static_assert(sizeof(nabla_cpuid_0000000b_t) == sizeof(nabla_cpuid_generic_t),
-               "struct nabla_cpuid_0000000b_t must be exactly of the same size as nabla_cpuid_generic_t!");
-_Static_assert(sizeof(nabla_cpuid_80000001_amd_t) == sizeof(nabla_cpuid_generic_t),
-               "struct nabla_cpuid_80000001_amd_t must be exactly of the same size as nabla_cpuid_generic_t!");
-_Static_assert(sizeof(nabla_cpuid_80000008_amd_t) == sizeof(nabla_cpuid_generic_t),
-               "struct nabla_cpuid_80000008_amd_t must be exactly of the same size as nabla_cpuid_generic_t!");
-_Static_assert(sizeof(nabla_cpuid_8000001d_amd_t) == sizeof(nabla_cpuid_generic_t),
-               "struct nabla_cpuid_8000001d_amd_t must be exactly of the same size as nabla_cpuid_generic_t!");
-_Static_assert(sizeof(nabla_cpuid_8000001e_amd_t) == sizeof(nabla_cpuid_generic_t),
-               "struct nabla_cpuid_8000001e_amd_t must be exactly of the same size as nabla_cpuid_generic_t!");
+_Static_assert(sizeof(nabla_x86_cpuid_generic_t) == 16,
+               "struct nabla_x86_cpuid_generic_t must be exactly 16 bytes!");
+_Static_assert(sizeof(nabla_x86_cpuid_0000000b_t) == sizeof(nabla_x86_cpuid_generic_t),
+               "struct nabla_x86_cpuid_0000000b_t must be exactly of the same size as nabla_x86_cpuid_generic_t!");
+_Static_assert(sizeof(nabla_x86_cpuid_80000001_amd_t) == sizeof(nabla_x86_cpuid_generic_t),
+               "struct nabla_x86_cpuid_80000001_amd_t must be exactly of the same size as nabla_x86_cpuid_generic_t!");
+_Static_assert(sizeof(nabla_x86_cpuid_80000008_amd_t) == sizeof(nabla_x86_cpuid_generic_t),
+               "struct nabla_x86_cpuid_80000008_amd_t must be exactly of the same size as nabla_x86_cpuid_generic_t!");
+_Static_assert(sizeof(nabla_x86_cpuid_8000001d_amd_t) == sizeof(nabla_x86_cpuid_generic_t),
+               "struct nabla_x86_cpuid_8000001d_amd_t must be exactly of the same size as nabla_x86_cpuid_generic_t!");
+_Static_assert(sizeof(nabla_x86_cpuid_8000001e_amd_t) == sizeof(nabla_x86_cpuid_generic_t),
+               "struct nabla_x86_cpuid_8000001e_amd_t must be exactly of the same size as nabla_x86_cpuid_generic_t!");
 
 
 
@@ -228,27 +228,40 @@ extern uint32_t    nabla_pext32(uint32_t v, uint32_t sel);
 extern uint32_t    nabla_lzcnt32(uint32_t v);
 extern uint32_t    nabla_tzcnt32(uint32_t v);
 extern uint32_t    nabla_popcnt32(uint32_t v);
-extern uint32_t    nabla_cpuid_x86(void* regs, uint32_t leaf, uint32_t subleaf, int sel);
-extern uint32_t    nabla_cpuid_x86_is_leaf_supported(uint32_t leaf);
-extern uint32_t    nabla_cpuid_x86_family(void);
-extern uint32_t    nabla_cpuid_x86_model(void);
-extern uint32_t    nabla_cpuid_x86_stepping(void);
-extern uint32_t    nabla_cpuid_x86_family_model_stepping(void);
-extern int         nabla_cpuid_x86_is_amd(void);
-extern int         nabla_cpuid_x86_is_intel(void);
-extern int         nabla_cpuid_x86_is_hygon(void);
-extern int         nabla_cpuid_x86_is_amd_or_hygon(void);
-extern const char* nabla_cpuid_x86_microarchitecture(void);
-extern uint32_t    nabla_cpuid_x86_apicid_initial(void);
-extern uint32_t    nabla_cpuid_x86_apicid_extended(void);
-extern uint32_t    nabla_cpuid_x86_apicid_mask_smt(void);
-extern uint32_t    nabla_cpuid_x86_apicid_mask_core_logical(void);
-extern uint32_t    nabla_cpuid_x86_apicid_mask_core_physical(void);
-extern uint32_t    nabla_cpuid_x86_apicid_mask_core_llc(void);
-extern uint32_t    nabla_cpuid_x86_apicid_mask_package(void);
-extern uint32_t    nabla_cpuid_x86_apicid_mask_amd_ccd(void);
-extern uint32_t    nabla_cpuid_x86_apicid_mask_amd_ccx(void);
+extern uint64_t    nabla_x86_rdtsc();
+extern uint64_t    nabla_x86_rdtscp();
+extern uint64_t    nabla_x86_rdtscp_pid(uint32_t* p);
+extern uint64_t    nabla_x86_rdpid();
+extern uint64_t    nabla_x86_rdpru(uint64_t v);
+extern uint32_t    nabla_x86_cpuid(void* regs, uint32_t leaf, uint32_t subleaf, int sel);
+extern uint32_t    nabla_x86_cpuid_is_leaf_supported(uint32_t leaf);
+extern uint32_t    nabla_x86_cpuid_family(void);
+extern uint32_t    nabla_x86_cpuid_model(void);
+extern uint32_t    nabla_x86_cpuid_stepping(void);
+extern uint32_t    nabla_x86_cpuid_family_model_stepping(void);
+extern int         nabla_x86_cpuid_is_amd(void);
+extern int         nabla_x86_cpuid_is_intel(void);
+extern int         nabla_x86_cpuid_is_hygon(void);
+extern int         nabla_x86_cpuid_is_amd_or_hygon(void);
+extern const char* nabla_x86_cpuid_microarchitecture(void);
+extern uint32_t    nabla_x86_cpuid_apicid_initial(void);
+extern uint32_t    nabla_x86_cpuid_apicid_extended(void);
+extern uint32_t    nabla_x86_cpuid_apicid_mask_smt(void);
+extern uint32_t    nabla_x86_cpuid_apicid_mask_core_logical(void);
+extern uint32_t    nabla_x86_cpuid_apicid_mask_core_physical(void);
+extern uint32_t    nabla_x86_cpuid_apicid_mask_core_llc(void);
+extern uint32_t    nabla_x86_cpuid_apicid_mask_package(void);
+extern uint32_t    nabla_x86_cpuid_apicid_mask_amd_ccd(void);
+extern uint32_t    nabla_x86_cpuid_apicid_mask_amd_ccx(void);
 
+extern int sgemm_frag(int transA, int transB,
+                      size_t M, size_t N, size_t K,
+                      float  alpha,
+                      float* A, ssize_t LDA,
+                      float* B, ssize_t LDB,
+                      float  beta,
+                      float* C, ssize_t LDC,
+                      float* L);
 
 
 /**
@@ -256,11 +269,15 @@ extern uint32_t    nabla_cpuid_x86_apicid_mask_amd_ccx(void);
  */
 
 int main(int argc, char* argv[]){
-    (void)argc;
-    (void)argv;
-    int i,j;
+    uint64_t Tsmperf, Tsaperf, Temperf, Teaperf;
+    uint64_t mperf, aperf;
     
-#if 1
+    int i,j;
+    int argn[6] = {0};
+    for(i=1;i<(int)(sizeof argn/sizeof *argn);i++)
+        argn[i] = i<argc ? strtoul(argv[i], 0, 0) : 0;
+    
+#if 0
     const int EAX=0, EBX=1, ECX=2, EDX=3;
     int       smt;
     char      procbrand[64];
@@ -268,39 +285,39 @@ int main(int argc, char* argv[]){
     uint32_t  initapicid, x2apicid, x2apicidsupport;
     uint32_t  masklvl, masksub, maskccx, maskccd,
               masksmt, masklog, maskphy, maskpkg;
-    nabla_cpuid_generic_t      manubrand;
-    nabla_cpuid_0000000b_t     topoleaf;
-    nabla_cpuid_80000001_amd_t featuresext;
-    nabla_cpuid_80000008_amd_t leafx08;
-    nabla_cpuid_8000001d_amd_t cachelv;
-    nabla_cpuid_8000001e_amd_t extapicid;
+    nabla_x86_cpuid_generic_t      manubrand;
+    nabla_x86_cpuid_0000000b_t     topoleaf;
+    nabla_x86_cpuid_80000001_amd_t featuresext;
+    nabla_x86_cpuid_80000008_amd_t leafx08;
+    nabla_x86_cpuid_8000001d_amd_t cachelv;
+    nabla_x86_cpuid_8000001e_amd_t extapicid;
     
-    if(!nabla_cpuid_x86_is_leaf_supported(1))
+    if(!nabla_x86_cpuid_is_leaf_supported(1))
         return fprintf(stderr, "No CPUID leaves!\n"), 1;
-    if(!nabla_cpuid_x86_is_leaf_supported(0x80000001))
+    if(!nabla_x86_cpuid_is_leaf_supported(0x80000001))
         return fprintf(stderr, "No CPUID extended leaves!\n"), 1;
     
-    nabla_cpuid_x86(&manubrand,    0,          0, EAX);
-    nabla_cpuid_x86(&featuresext,  0x80000001, 0, EAX);
-    nabla_cpuid_x86(procbrand+ 0,  0x80000002, 0, EAX);
-    nabla_cpuid_x86(procbrand+16,  0x80000003, 0, EBX);
-    nabla_cpuid_x86(procbrand+32,  0x80000004, 0, ECX);
-    nabla_cpuid_x86(procbrand+48,  0x80000005, 0, EDX);
-    nabla_cpuid_x86(&leafx08,      0x80000008, 0, EAX);
-    nabla_cpuid_x86(&extapicid,    0x8000001E, 0, EAX);
-    initapicid      = nabla_cpuid_x86_apicid_initial();
-    x2apicid        = nabla_cpuid_x86_apicid_extended();
-    x2apicidsupport = nabla_cpuid_x86_is_leaf_supported(0x0B) &&
-                      nabla_cpuid_x86(0, 0x0B, 0, EBX);
+    nabla_x86_cpuid(&manubrand,    0,          0, EAX);
+    nabla_x86_cpuid(&featuresext,  0x80000001, 0, EAX);
+    nabla_x86_cpuid(procbrand+ 0,  0x80000002, 0, EAX);
+    nabla_x86_cpuid(procbrand+16,  0x80000003, 0, EBX);
+    nabla_x86_cpuid(procbrand+32,  0x80000004, 0, ECX);
+    nabla_x86_cpuid(procbrand+48,  0x80000005, 0, EDX);
+    nabla_x86_cpuid(&leafx08,      0x80000008, 0, EAX);
+    nabla_x86_cpuid(&extapicid,    0x8000001E, 0, EAX);
+    initapicid      = nabla_x86_cpuid_apicid_initial();
+    x2apicid        = nabla_x86_cpuid_apicid_extended();
+    x2apicidsupport = nabla_x86_cpuid_is_leaf_supported(0x0B) &&
+                      nabla_x86_cpuid(0, 0x0B, 0, EBX);
     nabla_uint32tobin(x2apicidbin, x2apicid);
-    smt     = nabla_cpuid_x86_is_leaf_supported(0x8000001E) &&
+    smt     = nabla_x86_cpuid_is_leaf_supported(0x8000001E) &&
               extapicid.threads_per_core > 0;
-    masksmt = nabla_cpuid_x86_apicid_mask_smt();
-    masklog = nabla_cpuid_x86_apicid_mask_core_logical();
-    maskphy = nabla_cpuid_x86_apicid_mask_core_physical();
-    maskpkg = nabla_cpuid_x86_apicid_mask_package();
-    maskccx = nabla_cpuid_x86_apicid_mask_amd_ccx();
-    maskccd = nabla_cpuid_x86_apicid_mask_amd_ccd();
+    masksmt = nabla_x86_cpuid_apicid_mask_smt();
+    masklog = nabla_x86_cpuid_apicid_mask_core_logical();
+    maskphy = nabla_x86_cpuid_apicid_mask_core_physical();
+    maskpkg = nabla_x86_cpuid_apicid_mask_package();
+    maskccx = nabla_x86_cpuid_apicid_mask_amd_ccx();
+    maskccd = nabla_x86_cpuid_apicid_mask_amd_ccd();
     masksub = maskphy^maskccx^maskccd;
     
     fprintf(stdout,
@@ -322,16 +339,16 @@ int main(int argc, char* argv[]){
             (const char*)&manubrand.ebx,
             (const char*)&manubrand.edx,
             (const char*)&manubrand.ecx,
-            (nabla_cpuid_x86_is_amd()   ? " (AMD)"   :
-             nabla_cpuid_x86_is_intel() ? " (Intel)" :
-             nabla_cpuid_x86_is_hygon() ? " (Hygon)" : ""),
+            (nabla_x86_cpuid_is_amd()   ? " (AMD)"   :
+             nabla_x86_cpuid_is_intel() ? " (Intel)" :
+             nabla_x86_cpuid_is_hygon() ? " (Hygon)" : ""),
             procbrand,
-            nabla_cpuid_x86(0, 1, 0, EAX),
-            nabla_cpuid_x86_family(),   nabla_cpuid_x86_family(),
-            nabla_cpuid_x86_model(),    nabla_cpuid_x86_model(),
-            nabla_cpuid_x86_stepping(), nabla_cpuid_x86_microarchitecture(),
-            nabla_cpuid_x86_is_leaf_supported(0),
-            nabla_cpuid_x86_is_leaf_supported(0x80000000),
+            nabla_x86_cpuid(0, 1, 0, EAX),
+            nabla_x86_cpuid_family(),   nabla_x86_cpuid_family(),
+            nabla_x86_cpuid_model(),    nabla_x86_cpuid_model(),
+            nabla_x86_cpuid_stepping(), nabla_x86_cpuid_microarchitecture(),
+            nabla_x86_cpuid_is_leaf_supported(0),
+            nabla_x86_cpuid_is_leaf_supported(0x80000000),
             smt+1, initapicid, x2apicid,
             x2apicidsupport ? " (x2APIC ID available)" : " (inferred as initial APIC ID)",
             nabla_pext32(x2apicid, masksmt), masksmt,
@@ -339,15 +356,15 @@ int main(int argc, char* argv[]){
             nabla_pext32(x2apicid, maskphy), maskphy,
             nabla_pext32(x2apicid, maskpkg), maskpkg);
     
-    if(nabla_cpuid_x86_is_amd_or_hygon())
+    if(nabla_x86_cpuid_is_amd_or_hygon())
         fprintf(stdout,
                 "APIC AMD CCD ID:               %-10u (mask: %08x)\n"
                 "APIC AMD CCX ID:               %-10u (mask: %08x)\n",
                 nabla_pext32(x2apicid, maskccd), maskccd,
                 nabla_pext32(x2apicid, maskccx), maskccx);
     
-    if(nabla_cpuid_x86_is_leaf_supported(0x0B)){
-        for(i=j=0; nabla_cpuid_x86(&topoleaf, 0x0B, i, 0),
+    if(nabla_x86_cpuid_is_leaf_supported(0x0B)){
+        for(i=j=0; nabla_x86_cpuid(&topoleaf, 0x0B, i, 0),
                    topoleaf.level_type; i++,j=topoleaf.next_level_shift){
             if(!i)
                 fprintf(stdout,
@@ -370,7 +387,7 @@ int main(int argc, char* argv[]){
                     nabla_tzcnt32(masklvl), "",
                     nabla_pext32(x2apicid, masklvl));
             
-            if(nabla_cpuid_x86_is_amd_or_hygon() &&
+            if(nabla_x86_cpuid_is_amd_or_hygon() &&
                topoleaf.level_type == 2){
                 if(masksub && (maskccx || maskccd))
                     fprintf(stdout,
@@ -411,9 +428,9 @@ int main(int argc, char* argv[]){
         }
     }
     
-    if(nabla_cpuid_x86_is_amd_or_hygon() &&
-       nabla_cpuid_x86_is_leaf_supported(0x8000001D)){
-        for(i=0; nabla_cpuid_x86(&cachelv, 0x8000001D, i, 0), cachelv.type; i++){
+    if(nabla_x86_cpuid_is_amd_or_hygon() &&
+       nabla_x86_cpuid_is_leaf_supported(0x8000001D)){
+        for(i=0; nabla_x86_cpuid(&cachelv, 0x8000001D, i, 0), cachelv.type; i++){
             if(!i)
                 fprintf(stdout,
                         "CACHE HIERARCHY\n"
@@ -454,38 +471,66 @@ int main(int argc, char* argv[]){
     madvise(L, Lsz, MADV_RANDOM);
     mlock  (L, Lsz);
     
-    float (*A)[64]  = (void*)(L + 0);  /* float A[400][ 64]; */
-    float (*B)[400] = (void*)(A + 400);/* float B[ 64][400]; */
-    float (*C)[400] = (void*)(B + 64); /* float C[400][400]; */
+    float (*A)[128] = (void*)(L + 0);  /* float A[160][128]; */
+    float (*B)[160] = (void*)(A + 160);/* float B[128][160]; */
+    float (*C)[160] = (void*)(B + 128);/* float C[160][160]; */
     
-    for(i=0;i<400;i++){
-        for(j=0;j<64;j++){
-            A[i][j] = 1.0f;    /* A is ones() */
-            B[j][i] = (j+i)&63;/* B is circulant(range(64)) */
+    for(i=0;i<160;i++){
+        for(j=0;j<128;j++){
+            A[i][j] = 1.0f;     /* A is ones() */
+            B[j][i] = (j+i)&127;/* B is circulant(range(127)) */
         }
-        for(j=0;j<400;j++){
-            C[j][i] = 0;       /* C is zeroes() */
+        for(j=0;j<160;j++){
+            C[j][i] = 0;        /* C is zeroes() */
         }
     }
     
-    extern int sgemm_frag(int transA, int transB,
-                          size_t M, size_t N, size_t K,
-                          float  alpha,
-                          float* A, ssize_t LDA,
-                          float* B, ssize_t LDB,
-                          float  beta,
-                          float* C, ssize_t LDC,
-                          float* L);
     extern void sgemm_test(float* A, float* B, float* C,
                            int K0, int counter, int prefetch_counter, float alpha, float beta);
     
-    for(i=0;i<10000;i++){
-        sgemm_test(L+0, L+400*64, L+2*400*64,                        /* A, B & C pointers */
-                            32-5<<18 | 32-5<<13 | 16-5<<9 | 16-8<<5, /* K0                */
-                   15<<23 |    4<<18 |    4<<13 |    4<<9,           /* counter           */
-                   15<<23 |    4<<18 |    4<<13 |    4<<9 |    5<<5, /* prefetch counter  */
-                   1.0f, 0.0f);                                      /* alpha & beta      */
-    }
+    const unsigned long R  = (argn[1] ? argn[1] : 10000),
+                        P  = (argn[2] ? argn[2] :     2)-1,
+                        N  = (argn[3] ? argn[3] :     5)-1,
+                        M  = (argn[4] ? argn[4] :    32)-1,
+                        K  = (argn[5] ? argn[5] :     8)-1,
+                        fp = R*(M+1)*5 *(N+1)*(P+1)*16      *(K+1)*16*2, /* The x2 is for FMA = 2 FLOPS */
+                        cc = R*(M+1)   *(N+1)*(P+1)         *(K+1)*80;   /* The 80 is for 160 FMAs per loop iteration @ 2 FMA/cc */
+    (void)fp;
+    (void)cc;
     
+#if 0
+    printf("R:                   %lu\n"
+           "M:                   %lu\n"
+           "N:                   %lu (P: %lu)\n"
+           "K:                   %lu\n",
+           R, (M+1)*5, (N+1)*(P+1)*16, P+1, (K+1)*8);
+#endif
+#if NABLA_AMD
+    Tsmperf = nabla_x86_rdpru(0);
+    Tsaperf = nabla_x86_rdpru(1);
+#endif
+    for(i=0;i<(int)R;i++){
+        int K0   = (           (63^M) << 8 | (3^P)<< 6 | (15^K) << 2);
+        int cnt  = ( N << 14 |     M  << 8 |    P << 6 |     K  << 2);
+        int pcnt = ( N << 14 | (-0+M) << 8 |    0 << 6 |     K  << 2);
+        sgemm_test(*A, *B, *C, K0, cnt, pcnt, 1.0f, 0.0f);
+    }
+#if NABLA_AMD
+    Temperf = nabla_x86_rdpru(0);
+    Teaperf = nabla_x86_rdpru(1);
+    mperf   = (Temperf-Tsmperf) & 0xFFFFFFFFFFFFULL;
+    aperf   = (Teaperf-Tsaperf) & 0xFFFFFFFFFFFFULL;
+    printf("FLOPS:              %15lu\n"
+           "Expected  Zen 2 cc: %15lu\n"
+           "Actual    Zen 2 cc: %15lu (+%4.2f%%, eff: %5.2f%%)\n"
+           "Wallclock Zen 2 cc: %15lu\n"
+           "Overhead: %.2f cc/BU of %lu cc\n",
+           fp, cc, aperf,
+           100.0*(aperf-cc)/cc,
+           100.0*cc/aperf,
+           mperf,
+           (aperf-cc)/(fp/(160*16.0*(K+1))),
+           (K+1)*80);
+#endif
     return 0;
 }
